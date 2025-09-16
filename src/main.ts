df0c24f35585435ba943166337ab4af0dd3fda9f
Marcos Vinicius Mulinari <marcos.mulinari97@gmail.com>
2025-09-16 12:10:21 -0300

    feat: habilitado o cors para a porta 4200

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+   //habilita o cors para a porta 4200
+   app.enableCors({
+      origin: 'http://localhost:4200',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept, Authorization',
+   });
 
   app.useGlobalPipes(new ValidationPipe({
      whitelist: true,
